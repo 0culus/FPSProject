8d446b827bd11b0b7f716e6a82d87cd0ab80601c
Sean Turner <sturner@nmt.edu>
2015-06-12 22:27:38 -0600

    add projectile ballistics stuff

--- Source/FPSProject/FPSProjectile.cpp
+++ Source/FPSProject/FPSProjectile.cpp
@@ -12,6 +12,24 @@ AFPSProjectile::AFPSProjectile()
 
 }
 
+AFPSProjectile::AFPSProjectile(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	// Use a sphere as a simple collision representation
+	CollisionComp = ObjectInitializer.CreateDefaultSubobject<USphereComponent>(this, TEXT("SphereComp"));
+	CollisionComp->InitSphereRadius(15.0f);
+	RootComponent = CollisionComp;
+
+	// Use a ProjectileMovementComponent to govern this projectile's movement
+	ProjectileMovement = ObjectInitializer.CreateDefaultSubobject<UProjectileMovementComponent>(this, TEXT("ProjectileComp"));
+	ProjectileMovement->UpdatedComponent = CollisionComp;
+	ProjectileMovement->InitialSpeed = 3000.f;
+	ProjectileMovement->MaxSpeed = 3000.f;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = true;
+	ProjectileMovement->Bounciness = 0.3f;
+}
+
 // Called when the game starts or when spawned
 void AFPSProjectile::BeginPlay()
 {
@@ -26,3 +44,11 @@ void AFPSProjectile::Tick( float DeltaTime )
 
 }
 
+void AFPSProjectile::InitVelocity(const FVector& ShootDirection)
+{
+	if (ProjectileMovement)
+	{
+		// set the projectile's velocity to the desired direction
+		ProjectileMovement->Velocity = ShootDirection * ProjectileMovement->InitialSpeed;
+	}
+}
--- Source/FPSProject/FPSProjectile.h
+++ Source/FPSProject/FPSProjectile.h
@@ -11,8 +11,18 @@ class FPSPROJECT_API AFPSProjectile : public AActor
 	GENERATED_BODY()
 	
 public:	
+	/** Sphere collision component */
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+	USphereComponent* CollisionComp;
+
+	/** Projectile movement component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement)
+	UProjectileMovementComponent* ProjectileMovement;
+
 	// Sets default values for this actor's properties
 	AFPSProjectile();
+
+	AFPSProjectile(const FObjectInitializer& ObjectInitializer);
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -20,6 +30,7 @@ public:
 	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
 
-	
-	
+	// Set velocity of projectile in shot direction
+	void InitVelocity(const FVector& ShootDirection);
+
 };
